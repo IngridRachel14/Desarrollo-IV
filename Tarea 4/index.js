edd932d0c160252812a7e5b2275f62d18a06c1e4
DevJohnG <john.grant@utp.ac.pa>
2023-10-29 22:41:37 -0500

    Corregido 2
    Con alertas negativas

--- Tarea 4/index.js
+++ Tarea 4/index.js
@@ -1,23 +1,21 @@
-var contra = document.getElementById('Contra');
-var ojo = document.getElementById('Ojo');
-
-ojo.addEventListener("click", e => {
-    if (contra.type === "password") {
-        contra.type = "text";
-        ojo.style.opacity = 0.9;
-    }
-    else {
-        contra.type = "password"
-        ojo.style.opacity = 0.3;
-    }
-})
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
+    var contra = document.getElementById('Contra');
+    var ojo = document.getElementById('Ojo');
     var mailInput = document.querySelector('.Mail');
     var contraInput = document.getElementById('Contra');
     var enviarBtn = document.querySelector('.Enviar');
 
-    enviarBtn.addEventListener('click', function (event) {
+    ojo.addEventListener("click", function() {
+        if (contra.type === "password") {
+            contra.type = "text";
+            ojo.style.opacity = 0.9;
+        } else {
+            contra.type = "password";
+            ojo.style.opacity = 0.3;
+        }
+    });
+
+    enviarBtn.addEventListener('click', function(event) {
         if (mailInput.value === '') {
             mailInput.style.backgroundColor = 'red';
             event.preventDefault();
@@ -31,18 +29,21 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             contraInput.style.backgroundColor = '';
         }
-    });
-});
 
-enviarBtn.addEventListener('click', function (event) {
-    if (mailInput.value === '' && contraInput.value === '') {
-        alert('Por favor, completa los campos de correo electrónico y contraseña.');
-        event.preventDefault();
-    } else if (mailInput.value === '') {
-        alert('Por favor, ingresa tu correo electrónico.');
-        event.preventDefault();
-    } else if (contraInput.value === '') {
-        alert('Por favor, ingresa tu contraseña.');
-        event.preventDefault();
-    }
+        if (mailInput.value === '' && contraInput.value === '') {
+            window.alert('Completa los campos de correo y contraseña');
+            event.preventDefault();
+        } else if (mailInput.value === '') {
+            window.alert('Ingresa tu correo electrónico.');
+            event.preventDefault();
+        } else if (contraInput.value === '') {
+            window.alert('Ingresa tu contraseña.');
+            event.preventDefault();
+        }
+
+        else{
+            Window.alert('BIENVENIDO A WARFARE')
+            event.preventDefault();
+        }
+    });
 });
