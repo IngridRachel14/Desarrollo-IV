ee14f3b3439073c8bf729e7e346a7a0656f7b972
IngridRachel14 <131417589+IngridRachel14@users.noreply.github.com>
2023-10-29 22:48:10 -0500

    logean

--- Tarea 4/index.js
+++ Tarea 4/index.js
@@ -1,21 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var contra = document.getElementById('Contra');
-    var ojo = document.getElementById('Ojo');
+var contra = document.getElementById('Contra');
+var ojo = document.getElementById('Ojo');
+
+ojo.addEventListener("click", e => {
+    if (contra.type === "password") {
+        contra.type = "text";
+        ojo.style.opacity = 0.9;
+    }
+    else {
+        contra.type = "password"
+        ojo.style.opacity = 0.3;
+    }
+})
+
+document.addEventListener("DOMContentLoaded", function () {
     var mailInput = document.querySelector('.Mail');
     var contraInput = document.getElementById('Contra');
     var enviarBtn = document.querySelector('.Enviar');
 
-    ojo.addEventListener("click", function() {
-        if (contra.type === "password") {
-            contra.type = "text";
-            ojo.style.opacity = 0.9;
-        } else {
-            contra.type = "password";
-            ojo.style.opacity = 0.3;
-        }
-    });
-
-    enviarBtn.addEventListener('click', function(event) {
+    enviarBtn.addEventListener('click', function (event) {
         if (mailInput.value === '') {
             mailInput.style.backgroundColor = 'red';
             event.preventDefault();
@@ -29,21 +31,34 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             contraInput.style.backgroundColor = '';
         }
-
-        if (mailInput.value === '' && contraInput.value === '') {
-            window.alert('Completa los campos de correo y contraseña');
-            event.preventDefault();
-        } else if (mailInput.value === '') {
-            window.alert('Ingresa tu correo electrónico.');
-            event.preventDefault();
-        } else if (contraInput.value === '') {
-            window.alert('Ingresa tu contraseña.');
-            event.preventDefault();
-        }
-
-        else{
-            Window.alert('BIENVENIDO A WARFARE')
-            event.preventDefault();
-        }
     });
 });
+
+enviarBtn.addEventListener('click', function (event) {
+    if (mailInput.value === '' && contraInput.value === '') {
+        alert('Por favor, completa los campos de correo electrónico y contraseña.');
+        event.preventDefault();
+    } else if (mailInput.value === '') {
+        alert('Por favor, ingresa tu correo electrónico.');
+        event.preventDefault();
+    } else if (contraInput.value === '') {
+        alert('Por favor, ingresa tu contraseña.');
+        event.preventDefault();
+    }
+});
+
+
+
+function showLoader() {
+    var loader = document.getElementById("loader");
+    loader.classList.remove("hidden");
+
+    var enviarButton = document.querySelector(".Enviar");
+    enviarButton.disabled = true;
+
+    setTimeout(function () {
+        
+        loader.classList.add("hidden");
+        enviarButton.disabled = false;
+    }, 3000);
+}
