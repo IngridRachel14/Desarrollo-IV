06440d027f5854c3ef57383101af04419e580303
DevJohnG <john.grant@utp.ac.pa>
2023-10-29 21:52:00 -0500

    JS del ojo

--- Tarea 4/index.js
+++ Tarea 4/index.js
@@ -1,2 +1,13 @@
-const contra = document.getElementById("contra"),
-    icono = document.querySelector("bx")
+var contra = document.getElementById('Contra');
+var ojo = document.getElementById('Ojo');
+
+    ojo.addEventListener("click", e => {
+        if (contra.type === "password"){
+            contra.type = "text";
+            ojo.style.opacity = 0.9;
+        }
+        else{
+            contra.type = "password"
+            ojo.style.opacity = 0.3;
+        }
+    })
